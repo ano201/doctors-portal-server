978f9fa3744bbeb1083d132ee5f3fe40039e6ba2
ano201 <ahmu689@gmail.com>
2021-11-10 00:43:12 +0600

    date error fixed

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
 
         app.get('/appointments', async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             const query = { email: email, date: date }
             const cursor = appointmentCollection.find(query);
             const appointments = await cursor.toArray();
